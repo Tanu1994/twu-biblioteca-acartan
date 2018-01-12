16432e9134e30fc25a1708c7cc03625f49d8ab3b
Andrei-Alexandru Cartan <andrei-alexandrucartan@Andrei-Alexandrus-MacBook-Pro.local>
2018-01-12 13:16:54 +0000

    Updated menu and option to print the list of books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -7,7 +7,17 @@ public class BibliotecaApp {
     public static void main(String[] args) {
 
         Menu menu = new Menu();
+        Book[] books = {
+                new Book("Harry Potter", "J.K.Rowling", 2002),
+                new Book("The Kite Runner", "Khaled Hosseini", 2014),
+                new Book("Lolita", "Vladimir Nabokov", 2013),
+                new Book("Alice's Adventures in Wonderland", "Lewis Carroll", 2016),
+                new Book("The Great Gatsby", "F Scott Fitzgerald", 2007),
+                new Book("Vanity Fair", "William Thackery", 2011),
+                new Book("The Hobbit", "JRR Tolkien", 2015),
+        };
         Scanner in = new Scanner(System.in);
+
 
         System.out.println(menu.WelcomeMessage());
         System.out.println();
@@ -18,12 +28,13 @@ public class BibliotecaApp {
 
         do{
             System.out.println("1. See a list of books");
-            System.out.println("2. Return a book");
-            System.out.println("3. Quit");
+            System.out.println("2. Checkout a book");
+            System.out.println("3. Return a book");
+            System.out.println("4. Quit");
             selection = in.nextInt();
 
-            menu.doAction(selection);
+            menu.doAction(selection, books);
         }
-        while (selection != 3);
+        while (selection != 4);
     }
 }
--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -2,20 +2,42 @@ package com.twu.biblioteca;
 
 public class Menu {
 
+    private Book[] books;
 
     public String WelcomeMessage() {
         return "Welcome to Biblioteca! Our library is available.";
     }
 
-    public void doAction(int selection) {
+    public void doAction(int selection, Book[] books) {
         switch (selection){
-            case 3: break;
+            case 1: {
+                printListOfBooks(books);
+                System.out.println();
+                break;
+            }
+            case 2: {
+                returnBook();
+                break;
+            }
+            case 4: break;
             default:
             {
                 System.out.println();
                 System.out.println("Sorry, this is not a valid option. Please select another option.");
                 break;
             }
+        }
+    }
+
+    private void returnBook() {
+
+    }
+
+    private void printListOfBooks(Book[] books) {
+        System.out.println("List of available books:");
+        System.out.println();
+        for (int i = 0; i < books.length; i++){
+            System.out.println(books[i].getName() + " " + books[i].getAuthor() + " " + books[i].getYear());
         }
     }
 }
